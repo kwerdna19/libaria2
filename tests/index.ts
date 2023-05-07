d6c21235d611f1f82ff2c49bc9b7e86bcbdb675b
XLor <yjl9903@vip.qq.com>
2023-05-07 14:54:43 +0800

    chore: update test

--- tests/index.ts
+++ tests/index.ts
@@ -1,19 +1,19 @@
-import { WebSocket as Aria2WebSocket } from "../src/index";
+import { WebSocket as Aria2WebSocket } from '../src/index';
 
 (async () => {
   const client1 = new Aria2WebSocket.Client({
-    protocol: "ws",
-    host: "localhost",
+    protocol: 'ws',
+    host: 'localhost',
     port: 6800,
   });
 
-  // const gid1 = await client1.addUri(
-  //   "https://sharepoint.latest10.win/19042.804/19042.804.210131-1457.VB_RELEASE_SVC_PROD1_CLIENTMULTI_X64FRE_ZH-CN.ISO"
-  // );
-  console.log("added", gid1);
-  console.log(await client1.getVersion())
-  client1.on('aria2.onDownloadStart', ({gid}) => {
-     console.log(gid)
-  })
+  const gid1 = await client1.addUri(
+    'magnet:?xt=urn:btih:TYQHELYZVAJ5RHVER5VXO36V6XOSPBUO&dn=&tr=http%3A%2F%2F104.143.10.186%3A8000%2Fannounce&tr=udp%3A%2F%2F104.143.10.186%3A8000%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=http%3A%2F%2Ftracker3.itzmx.com%3A6961%2Fannounce&tr=http%3A%2F%2Ftracker4.itzmx.com%3A2710%2Fannounce&tr=http%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.prq.to%2Fannounce&tr=http%3A%2F%2Fopen.acgtracker.com%3A1096%2Fannounce&tr=https%3A%2F%2Ft-115.rhcloud.com%2Fonly_for_ylbud&tr=http%3A%2F%2Ftracker1.itzmx.com%3A8080%2Fannounce&tr=http%3A%2F%2Ftracker2.itzmx.com%3A6961%2Fannounce&tr=udp%3A%2F%2Ftracker1.itzmx.com%3A8080%2Fannounce&tr=udp%3A%2F%2Ftracker2.itzmx.com%3A6961%2Fannounce&tr=udp%3A%2F%2Ftracker3.itzmx.com%3A6961%2Fannounce&tr=udp%3A%2F%2Ftracker4.itzmx.com%3A2710%2Fannounce&tr=http%3A%2F%2Ftr.bangumi.moe%3A6969%2Fannounce&tr=http%3A%2F%2Ft.nyaatracker.com%2Fannounce&tr=http%3A%2F%2Fopen.nyaatorrents.info%3A6544%2Fannounce&tr=http%3A%2F%2Ft2.popgo.org%3A7456%2Fannonce&tr=http%3A%2F%2Fshare.camoe.cn%3A8080%2Fannounce&tr=http%3A%2F%2Fopentracker.acgnx.se%2Fannounce&tr=http%3A%2F%2Ftracker.acgnx.se%2Fannounce&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=http%3A%2F%2Ft.nyaatracker.com%3A80%2Fannounce&tr=https%3A%2F%2Ftr.bangumi.moe%3A9696%2Fannounce&tr=http%3A%2F%2Ft.acg.rip%3A6699%2Fannounce&tr=http%3A%2F%2Fopen.acgnxtracker.com%2Fannounce&tr=https%3A%2F%2Ftracker.nanoha.org%2Fannounce'
+  );
+  console.log('added', gid1);
+  console.log(await client1.getVersion());
+  client1.on('aria2.onDownloadStart', ({ gid }) => {
+    console.log(gid);
+  });
   client1.close();
 })();
